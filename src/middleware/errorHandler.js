d7b5c61e5c7612ffc8430acc4a39b19c6ea48184
Stepan_Venhzhyn <ids3688@gmail.com>
2026-01-19 09:49:07 +0200

    fix

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,13 +1,14 @@
-import createHttpError from 'http-errors';
+import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, _next) => {
   let statusCode = 500;
   let message = 'Server error';
 
-  if (createHttpError.isHttpError(err)) {
-    statusCode = err.statusCode;
+  if (err instanceof HttpError) {
+    statusCode = err.status;
     message = err.message;
   }
 
   res.status(statusCode).json({ message });
 };
+
